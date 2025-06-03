39a7ee1bb42f1f291920a9ea9fa89e80431ff426
cobbygraves <rmeocnis@hotmail.com>
2025-06-03 17:09:07 +0000

    added  routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { ActionsComponent } from './actions/actions.component';
-import { NoInvoiceComponent } from './no-invoice/no-invoice.component';
-import Invoices from '../assets/data.json';
-import { InvoiceCardComponent } from './invoice-card/invoice-card.component';
-import moment from 'moment';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-    HeaderComponent,
-    ActionsComponent,
-    NoInvoiceComponent,
-    InvoiceCardComponent,
+    HeaderComponent, RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'invoice';
-  invoices = Invoices.map((item) => ({
-    ...item,
-    paymentDue: moment(item.paymentDue).format('D MMM YYYY'),
-  }));
+
 }
